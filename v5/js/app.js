72a6041b9e94b779eeeb26a04760831f8ee69f69
urinx <1336006643@qq.com>
2015-10-10 19:47:46 +0800

    update self website

---
+++ v5/js/app.js
@@ -0,0 +1,282 @@
+var works = [
+	{
+		'title': 'WriteTyper',
+		'subTitle': '復古打字機 — Mac OS X App',
+		'website': 'http://urinx.github.io/app/writetyper',
+		'github': 'https://github.com/Urinx/WriteTyper',
+		'img': 'https://github.com/Urinx/WriteTyper/raw/master/screenshot/home.png',
+		'description': 'It can make old typewriter sounds when you keypress, let you more enjoy the time of coding or writing.',
+	},
+	{
+		'title': 'Device 9',
+		'subTitle': '设备信息显示 - iOS App',
+		'github': 'https://github.com/Urinx/Device-9',
+		'img': 'img/device9.png',
+		'description': '实时监测网速，IP，内存大小，温度等设备信息并显示在通知中心的 iOS App',
+	},
+	{
+		'title': 'WeixinWalk',
+		'subTitle': '微信运动刷榜神器',
+		'img': 'img/weixinwalk.png',
+		'description': '方法已失效',
+	},
+	{
+		'title': 'Vu',
+		'subTitle': '唯舞 - 街舞视频 iOS App',
+		'website': 'http://www.vhiphop.com/',
+		'github': 'https://github.com/Urinx/Vu',
+		'img': 'https://github.com/Urinx/Vu/raw/master/Screenshot/App_Screenshot.png',
+		'description': '小人常足不出户，虽闲情寡欲，但尤好赏舞，子曰：唯舞独尊。若于街市中观舞，余自幼体弱多病，出门远行，累觉不爱。幸有一网站，名之唯舞，上有万千视频多不胜数随君把玩，卧榻倚床，每有会意便欣然忘食，不亦乐乎。然每访其网站，观其视频，日久天长，实感不便矣。久寻客户端不得，怅然不知所以，遂做此软体，方便自己，亦益于他人。以上。',
+	},
+	{
+		'title': 'Iconista',
+		'subTitle': 'Mac主题图标美化工具',
+		'website': 'http://urinx.github.io/app/iconista/',
+		'github': 'https://github.com/Urinx/Iconista',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r59NVePQypWtyqQ1oqM5IfZM2PvWiarGZE1M8kkUISvMIs32Sialib7sGzTw8TP2cafQ0VtLlcqa8BiaQ/0?wx_fmt=jpeg',
+		'description': "It's easy, save, and fast tool that helps you to change your Mac Os X default applications icon, for it looks like horrible and ugly.",
+	},
+	{
+		'title': 'Muzik.js',
+		'subTitle': '音乐可视化库',
+		'website': 'http://urinx.github.io/app/muzik-js/',
+		'github': 'https://github.com/Urinx/Muzik.js',
+		'img': 'img/muzikjs.png',
+		'description': 'Show the frequency spectrum of your music in web page.',
+	},
+	{
+		'title': 'Card Player',
+		'subTitle': 'A HTML5 card music player',
+		'website': 'http://urinx.github.io/app/wechat/components/music-player/app.js',
+		'img': 'img/card-player.png',
+		'description': '一个基于Web Components开发的卡片式音乐播放器组件，可方便的整合进你的web应用中。',
+	},
+	{
+		'title': 'browspy.js',
+		'subTitle': '浏览器用户全部信息收集',
+		'github': 'https://github.com/Urinx/browspy',
+		'img': 'img/browspyjs.png',
+		'description': '这是一个通过html5,javascript用于收集用户通过上网泄漏的各种信息，包括地理位置，IP地址，照片，语音，浏览器版本等信息。结合大数据，可实现广告定向投放，用户追踪，用户行为分析，用户群体调研等一系列更人性化的服务。',
+	},
+	{
+		'title': 'WeChat Background',
+		'subTitle': 'SAE上基于PHP的微信后台',
+		'github': 'https://github.com/Urinx/weChat_php',
+		'img': 'img/wechat_public.jpg',
+		'description': '自己微信公众号的后台代码，也欢迎大家关注我的微信公众号:urinx',
+	},
+	{
+		'title': 'Chilight.js',
+		'subTitle': '中文语法高亮',
+		'github': 'https://github.com/Urinx/chinese_highlight',
+		'img': 'img/chilight.png',
+		'description': '结合SAE的分词服务，将中文文本转成语法高亮。',
+	},
+];
+var articles = [
+	{
+		'title': 'Github Hacking',
+		'link': 'http://www.jianshu.com/p/d6b54f1d60f1',
+		'img': 'http://upload-images.jianshu.io/upload_images/266748-b5c36895ec28fcae.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+		'description': 'Github不仅能托管代码，还能对代码进行搜索，我们感受到了其便利的同时，也应该时刻注意，当你上传并公开你的代码时，一时大意，让某些敏感的配置信息文件等暴露于众。',
+	},
+	{
+		'title': 'WriteTyper - 在Mac上體驗舊式打字機的觸感',
+		'link': 'http://www.jianshu.com/p/993c1dd06901',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r4j8JRDiceemEDCnjxlcCSmVK8Oibl96PgMfcF1OibXN9rOKzWPzcib5VWX3FQUVNvPEkgL7cHr8JFBlg/0?wx_fmt=jpeg',
+		'description': '不加点世事难料，调不出生活的味道。',
+	},
+	{
+		'title': '再谈微信运动',
+		'link': 'http://www.jianshu.com/p/0fe7b5543806',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r6vHMHUz98lwTIwxiaxaKoCmaUHMABJPao3G03QwdyhIl0IcQfRpZfvK0vnLggY3tvIICdQLxZicdyQ/0?wx_fmt=jpeg',
+		'description': '现在对自己的健康作弊，最后伤害的还是自己，运动不在于多少，在于问心无愧，每天多走一步，用真实的数据说话。',
+	},
+	{
+		'title': '微信运动刷榜神器',
+		'link': 'http://mp.weixin.qq.com/s?__biz=MzA3MjAzMTgyMA==&mid=209449871&idx=1&sn=06bbca9da7b6989faf7d61a97819910c#rd',
+		'img': 'http://mmbiz.qpic.cn/mmbiz/icv3Y2jicj1r7NB4gAgRY451vsiaV92B4EChUUXGvtRkwCmgKvYe3lX1AibTRJFG1weDZcCzR4FgvKQybicaGNicj3jQ/640?wx_fmt=jpeg&wxfrom=5',
+		'description': '哎哟妈啊，我再也不跟你们这群小伙伴比走路了',
+	},
+	{
+		'title': '唯舞 - 街舞视频',
+		'link': 'http://www.jianshu.com/p/7f939715cbfe',
+		'img': 'http://upload-images.jianshu.io/upload_images/266748-e62bab68debca5f4.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+		'description': '小人常足不出户，虽闲情寡欲，但尤好赏舞，子曰：唯舞独尊。若于街市中观舞，余自幼体弱多病，出门远行，累觉不爱。幸有一网站，名之唯舞，上有万千视频多不胜数随君把玩，卧榻倚床，每有会意便欣然忘食，不亦乐乎。然每访其网站，观其视频，日久天长，实感不便矣。久寻客户端不得，怅然不知所以，遂做此软体，方便自己，亦益于他人。以上。',
+	},
+	{
+		'title': 'Iconista首页背后的故事',
+		'link': 'http://www.jianshu.com/p/60b4c165672a#rd',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r59NVePQypWtyqQ1oqM5IfZM2PvWiarGZE1M8kkUISvMIs32Sialib7sGzTw8TP2cafQ0VtLlcqa8BiaQ/0?wx_fmt=jpeg',
+		'description': '安静的图书馆<br>电台广播传来熟悉的旋律<br>——“当时有多少心愿，就有多少的残缺”<br>忽然一阵悸动<br>在这个离别的六月季，听着五月天',
+	},
+	{
+		'title': 'Iconista——Mac下最炫酷的主题图标美化工具',
+		'link': 'http://www.jianshu.com/p/fd4d358b2aaa#rd',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r7adzYd4aVnmXVicwhAn1iaYLyTPSe5VCyRTPzrg9NPuIY1EhlsME4XgmUG21TaZic8ibTNtdO3kKk6oQ/0?wx_fmt=jpeg',
+		'description': '总想着长大了的以后，<br>再来弥补以前的六一',
+	},
+	{
+		'title': '迷失在双摆混沌系统里',
+		'link': 'http://www.jianshu.com/p/a57bfd91e0a1#rd',
+		'img': 'img/article3.jpeg',
+		'description': '如果世界上曾经有那个人出现过，那么其他人都会变成将就，而我不愿意将就。',
+	},
+	{
+		'title': 'VPN桥梁动力学',
+		'link': 'https://github.com/Urinx/SomeCodes/blob/master/Articles/Weekly%20Bullshit/2015.4.9/README.md#rd?sukey=fc78a68049a14bb2a7d452b2fa5a1e900cbafb91f2ab25da2695698b4c050bfead90b7c2d62536b75c8491df99f69cac',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r5xPOCpGwKHOFyh1s7gCKmLEYchjmW3bDERyD5oicEjemTLZkibibUyYNObcRGuPe9TQLbd7yvTs7ZuQ/0',
+		'description': '曾经，我把你的身影，放进窗口，定格为风景；<br>放牧思想，浅笔静开，梦与痴，笑与泪，便于悄然中呢喃一怀沉香。',
+	},
+	{
+		'title': '聊聊 Web Components 组件化开发',
+		'link': 'https://github.com/Urinx/SomeCodes/blob/master/Web/shadowDOM.demo/demo/README.md#rd?sukey=fc78a68049a14bb25637b760fdeddf744fa0c717fffb6292d8b8aa50eea423e88de1174342d989eeb5e32f30fbae7a97',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r4u5ZJ6HPc2LzHHptY27uC4rZTN2ABNwEBQVjbbSynG2phAZhV9XQaC4jF5C7tc6u4CYm4A1TW0xw/0',
+		'description': '假使哪日你刹那迷踪不明，我亦曾对你有过一场动情。',
+	},
+	{
+		'title': '请原谅我放荡不羁的Javascript',
+		'link': 'https://github.com/Urinx/SomeCodes/blob/master/Web/js_brainfuck/README.md#rd?sukey=fc78a68049a14bb2181a4143ff14a53182bf8b937219d2cb1dd4964962c5e99e2fc156098671370cac46fef9c46d5ee8',
+		'img': 'img/article6.png',
+		'description': '见过这样写js的吗，不解释，就是任性。',
+	},
+	{
+		'title': '浅析Chrome保存的密码为啥可以随便看',
+		'link': 'https://github.com/Urinx/Psychic-meme/#rd?sukey=fc78a68049a14bb286a25bdadf25d66a348645016ac1f3a4c193fb91e64a7b7b73cd105194739c35d9a0cbc76a128098',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r6dtsgByJdHOSpEvdWjIX1IwoPIbLmAcAj9cZapv6q4wYLNgCZMasBLHQx9XHmQxZr5Wl32SXuWSw/0',
+		'description': '执着于理想，纯粹于当下。人生无处不飞花。',
+	},
+	{
+		'title': '进入你的保护模式',
+		'link': 'https://github.com/Urinx/SomeCodes/tree/master/Asm/Pmode#rd?sukey=fc78a68049a14bb27ef6187bc648440a603e7466a295a5f02e74396d0ef2d7cb540ca0a2504b869801a20ab1170339be',
+		'img': 'img/article8.png',
+		'description': '如果你也怀有一颗不作死不罢休的心，拯救全人类的情怀，欢迎投稿，吐槽也发表。我们的口号是“推翻人类暴政，世界属于三体”。',
+	},
+	{
+		'title': '那天，我用青光眼看美剧《天蝎》',
+		'link': 'https://github.com/Urinx/SomeCodes/tree/master/Python/GaussianFilter#rd?sukey=fc78a68049a14bb2028d098012f6c908d81e623577c58383f746419e1d24e507971615a91f2f7fdd0965695ad4f0623f',
+		'img': 'https://mmbiz.qlogo.cn/mmbiz/icv3Y2jicj1r5sZVoGctHcdgAyN5QVwicfJ0xIiaHr0Lk4ictWAAazSicfFtkxluA9MnGaaIgGAzagLr8UkvRBLzrtpQ/0',
+		'description': '前言：每周二更新的《天蝎》，作为一部备受好评的秋季档神剧，第一集的横空出世确实表现不错，令人眼前一亮，奈何后劲不足。。只见满屏幕在闪，手速跟不上码速。',
+	},
+];
+
+var works_row = 3,
+	works_col = 3,
+	articles_row = 5,
+	articles_col = 3;
+
+var work_template = '\
+<div class="col s12 m4">\
+	<div class="card hoverable">\
+		<div class="card-image waves-effect waves-block waves-light">\
+			<img class="activator" src="###_IMG_###">\
+		</div>\
+		<div class="card-content center">\
+			<span class="card-title activator grey-text text-darken-4">###_TITLE_###</span>\
+			<p>###_SUBTITLE_###</p>\
+			<p class="right-align">\
+				###_IF1_###<a href="###_WEBSITE_###" target="_blank">Website</a>&nbsp;###_FI1_###\
+				###_IF2_###<a href="###_GITHUB_###" target="_blank">Github</a>###_FI2_###\
+			</p>\
+		</div>\
+		<div class="card-reveal">\
+			<span class="card-title grey-text text-darken-4">###_TITLE_###<i class="material-icons right">close</i></span>\
+			<p>###_DESCRIPTION_###</p>\
+		</div>\
+	</div>\
+</div>';
+
+var article_template = '\
+<div class="col s12 m4">\
+	<div class="card hoverable">\
+		<div class="card-image waves-effect waves-block waves-light">\
+			<img class="activator" src="###_IMG_###">\
+		</div>\
+		<div class="card-content">\
+			<span class="card-title activator grey-text text-darken-4">###_TITLE_###</span>\
+			<p class="right-align"><a href="###_LINK_###" target="_blank">阅读原文</a></p>\
+		</div>\
+		<div class="card-reveal">\
+			<span class="card-title grey-text text-darken-4">###_TITLE_###<i class="material-icons right">close</i></span>\
+			<p>###_DESCRIPTION_###</p>\
+		</div>\
+	</div>\
+</div>';
+
+
+(function($){
+	$(function(){
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+	});
+})(jQuery);
+
+$(document).ready(function(){
+	// 锚点平滑跳转
+	$('nav li a').click(function(){
+		$("html,body").animate({scrollTop:$("#"+this.text.toLowerCase()).offset().top},2000);
+	});
+	$('a.down-icon').click(function(){
+		$("html,body").animate({scrollTop:$("#terminal").offset().top},500);
+	});
+
+	// Set work content
+	var section = '';
+	for (var i = 0; i < works_row; i++) {
+		var row = '<div class="row">';
+		for (var j = 0; j < works_col; j++) {
+			var work = works[i * works_col + j];
+			if (work) {
+			var temp = work_template;
+				for (key in work) {
+					temp = temp.replace(eval('/###_'+key.toUpperCase()+'_###/g'),work[key]);
+				}
+				if (work['website']) {
+					temp = temp.replace('###_IF1_###','');
+					temp = temp.replace('###_FI1_###','');
+				} else {
+					temp = temp.replace('###_IF1_###','<!-- ');
+					temp = temp.replace('###_FI1_###',' -->');
+				}
+				if (work['github']) {
+					temp = temp.replace('###_IF2_###','');
+					temp = temp.replace('###_FI2_###','');
+				} else {
+					temp = temp.replace('###_IF2_###','<!-- ');
+					temp = temp.replace('###_FI2_###',' -->');
+				}
+				row += temp;
+			}
+		}
+		row += '</div>';
+		section += row;
+	}
+	var html = '<div class="container"><div class="section">'+section+'</div></div>';
+	$('works').html(html);
+
+	// Set article content
+	var section = '';
+	for (var i = 0; i < articles_row; i++) {
+		var row = '<div class="row">';
+		for (var j = 0; j < articles_col; j++) {
+			var article = articles[i * articles_col + j];
+			if (article) {
+				var temp = article_template;
+				for (key in article) {
+					temp = temp.replace(eval('/###_'+key.toUpperCase()+'_###/g'),article[key]);
+				}
+				row += temp;
+			}
+		}
+		row += '</div>';
+		section += row;
+	}
+	var html = '<div class="container"><div class="section">'+section+'</div></div>';
+	$('articles').html(html);
+});
+
+
+
+
+
+
